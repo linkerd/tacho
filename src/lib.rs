00818468252c9ef3e3dc65898a98de469009a689
Oliver Gould <ver@buoyant.io>
2017-03-27 16:46:57 +0000

    Support Prometheus-style labels, arbitrary histograms (#4)
    This is a pretty major overhaul of the public API.
    
    The `Tacho` type holds a metrics pipeline:
    
    `Metrics` provides a closeable, scopable handle for applications to produce
    metrics. It provides a `Scope`, which creates labeled `MetricKey`s. These keys
    identify a metric and its type, but accept no values.  Keys are to be
    initialized early in program execution, and may be cloned cheaply (without
    copying the underling labels). `Metrics` also produces RAII-guarded `Recorder`
    instances. Values are passed to the the recorder with a key. When the recorder
    instance is dropped, a `Sample` is sent on the recorder's mpsc channel.
    
    An `Aggregator` processes a stream of `Sample`s cooperatively (i.e. yielding
    back to the reactor after processing a batch of samples). Finally, the
    aggregator publishes a `Report`.  The consumer of this `Report` may reset it as
    it sees fit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,165 +1,94 @@
-//! `Future`-powered Metrics collection and reporting
+//! A thread-safe, `Future`-aware metrics library.
 //!
+//! Many programs need to information about runtime performance: the number of requests
+//! served, a distribution of request latency, the number of failures, the number of loop
+//! iterations, etc. `tacho` allows application code to record runtime information to a
+//! central `Aggregator` that merges data into a `Report`.
 //!
-//! Example use within a mixed multi-threaded app with tokio
+//! ## Performance
 //!
-//! ```no_run
-//! extern crate log;
-//! extern crate env_logger;
-//! extern crate futures;
-//! extern crate tacho;
-//! extern crate tokio_core;
-//! extern crate tokio_timer;
+//! We found that the default (cryptographic) `Hash` algorithm adds a significant
+//! performance penalty. So the non-cryptographic `RandomXxHashBuilder` algorithm is used
+//! by `Reporter` and `Sample`.
 //!
-//! use futures::Stream;
-//! use std::io::{Error, ErrorKind};
-//! use std::time::Duration;
-//! use std::thread;
-//! use tokio_core::reactor::Core;
-//! use tokio_timer::Timer as TokioTimer;
+//! Labels are stored in a `BTreeMap`, because they are used as keys in the `Report`'s
+//! `HashMap` (and so we need to be able to derive `Hash` on the set of labels).
 //!
-//! use tacho::{Counter, Gauge};
-//! use tacho::timer::Timer;
-//! use tacho::metrics;
-//!
-//! // A performance test for an asynchronous Metrics reporter with timers, counters, and gauges.
-//! fn main() {
-//!     drop(env_logger::init());
-//!
-//!     let (recorder, aggregator) = metrics::new();
-//!
-//!     let work_thread = {
-//!         let mut tx = recorder.clone();
-//!         let mut total_timer = Timer::new("total_time_us".to_owned());
-//!         thread::spawn(move || {
-//!             for i in 0..100_000_000 {
-//!                 if i % 100 == 0 {
-//!                     thread::sleep(Duration::from_millis(1));
-//!                 }
-//!                 let mut loop_timer = Timer::new("loop_timer_us".to_owned());
-//!                 let mut loop_counter = Counter::new("total_loops".to_owned(), 0);
-//!                 let loop_gauge = Gauge::new("loop_iter".to_owned(), i);
-//!                 loop_timer.start();
-//!                 loop_counter.incr(1);
-//!                 // Do your work here
-//!                 loop_timer.stop();
-//!                 tx.record(vec![loop_counter], vec![loop_gauge], vec![loop_timer]);
-//!             }
-//!             total_timer.stop();
-//!             tx.record(vec![], vec![], vec![total_timer])
-//!         })
-//!     };
-//!
-//!     let mut core = Core::new().expect("Failed to create core");
-//!     let handle = core.handle();
-//!     let (aggregated, aggregating) = aggregator.aggregate();
-//!     handle.spawn(aggregating);
-//!     handle.spawn(metrics::report_generator(aggregated));
-//!
-//!     let mut tx = recorder.clone();
-//!     let mut heartbeats = 0;
-//!     let heartbeater = TokioTimer::default()
-//!         .interval(Duration::from_millis(1000))
-//!         .map_err(|_| Error::new(ErrorKind::Other, "unable to run heartbeat"))
-//!         .for_each(|_| {
-//!             heartbeats += 1;
-//!             let heartbeats_gauge = Gauge::new("heartbeats".to_owned(), heartbeats);
-//!             tx.record(vec![], vec![heartbeats_gauge], vec![]);
-//!             Ok(()) as Result<(), std::io::Error>
-//!         });
-//!     core.run(heartbeater).expect("heartbeat failed");
-//!     work_thread.join().expect("work thread failed to join");
-//! }
-//! ```
+//! At times, metric keys must be cloned---specifically, when creating a new entry in a
+//! `Sample` or `Report`.
 
-extern crate chrono;
 extern crate futures;
 extern crate hdrsample;
-extern crate tokio_core;
 extern crate tokio_timer;
 #[macro_use]
 extern crate log;
 extern crate twox_hash;
 
-use std::collections::VecDeque;
+use futures::sync::{BiLock, mpsc};
 
-pub mod metrics;
-pub mod reporter;
-pub mod timer;
+mod aggregator;
+mod key;
+pub mod prometheus;
+mod recorder;
+mod scope;
+mod timing;
 
-pub use metrics::{Metrics, Recorder};
-pub use timer::Timer as Timed;
+pub use aggregator::{Aggregator, Report};
+pub use key::{CounterKey, GaugeKey, StatKey, MetricKey};
+pub use recorder::{Recorder, RecorderFactory, Sample};
+pub use scope::Scope;
+pub use timing::Timing;
 
-// Counters are monotonically increasing values.
-#[derive(Clone, Debug)]
-pub struct Counter {
-    pub name: String,
-    pub value: u64,
+/// Limits the maximum number of `Samples` processed in a single invocation of `poll()`.
+const AGGREGATOR_BATCH_SIZE: usize = 1_000;
+
+/// A metrics pipeline.
+///
+/// Metrics are to be written into a `Receiver`, which sends raw data to a single
+/// `Aggregator`, which publishes a `Report`.
+pub struct Tacho {
+    pub metrics: Metrics,
+    pub aggregator: Aggregator,
+    pub report: BiLock<Report>,
 }
-
-impl Counter {
-    // Creates a Counter with a given name and initial value.
-    pub fn new(name: String, init: u64) -> Counter {
-        Counter {
-            name: name,
-            value: init,
+impl Default for Tacho {
+    fn default() -> Tacho {
+        Tacho::new(AGGREGATOR_BATCH_SIZE)
+    }
+}
+impl Tacho {
+    pub fn new(aggregator_batch_size: usize) -> Tacho {
+        let (samples_tx, samples_rx) = mpsc::unbounded();
+        let (report, agg_report) = BiLock::new(Report::default());
+        Tacho {
+            metrics: Metrics {
+                recorder: recorder::factory(samples_tx),
+                scope: Scope::default(),
+            },
+            aggregator: aggregator::new(samples_rx, agg_report, aggregator_batch_size),
+            report: report,
         }
     }
+}
 
-    pub fn incr(&mut self, n: u64) {
-        self.value += n;
+#[derive(Clone)]
+pub struct Metrics {
+    recorder: RecorderFactory,
+    scope: Scope,
+}
+impl Metrics {
+    pub fn recorder(&self) -> Recorder {
+        self.recorder.mk()
     }
-
-    pub fn fresh(&self) -> Counter {
-        Counter {
-            name: self.name.clone(),
-            value: 0,
+    pub fn scope(&self) -> &Scope {
+        &self.scope
+    }
+    pub fn labeled(self, k: String, v: String) -> Metrics {
+        let rec = self.recorder;
+        let scope = self.scope;
+        Metrics {
+            recorder: rec,
+            scope: scope.labeled(k, v),
         }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Gauge {
-    pub name: String,
-    pub value: u64,
-}
-
-impl Gauge {
-    pub fn new(name: String, n: u64) -> Gauge {
-        Gauge {
-            name: name,
-            value: n,
-        }
-    }
-
-    pub fn set(&mut self, n: u64) {
-        self.value = n;
-    }
-}
-
-pub struct Stat {
-    pub name: String,
-    pub values: VecDeque<u64>,
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn counter_incr() {
-        let mut counter = Counter::new("foo".to_owned(), 3);
-        counter.incr(1);
-        counter.incr(1);
-        counter.incr(1);
-        assert!(counter.value == 6);
-    }
-
-    #[test]
-    fn test_basic_gauges() {
-        let mut v = Gauge::new("foo".into(), 123);
-        v.set(432);
-        assert_eq!(v.value, 432);
     }
 }
---
+++ src/prometheus.rs
@@ -0,0 +1,77 @@
+use super::{Report, MetricKey};
+use std::collections::BTreeMap;
+use std::fmt::Display;
+
+// The initial size
+const BUF_SIZE: usize = 8 * 1024;
+
+/// Renders a `Report` for Prometheus.
+///
+/// ```
+/// ```
+pub fn format(report: &Report) -> String {
+    let mut out = String::with_capacity(BUF_SIZE);
+
+    out.push_str(&format!("metrics_count {}\n", report.len()));
+
+    for (k, v) in &report.counters {
+        let labels = k.labels();
+        if labels.is_empty() {
+            out.push_str(&format!("{} {}\n", k.name(), v));
+        } else {
+            out.push_str(&format!("{}{{{}}} {}\n", k.name(), &format_labels(labels), v));
+        }
+    }
+
+    for (k, v) in &report.gauges {
+        let labels = k.labels();
+        if labels.is_empty() {
+            out.push_str(&format!("{} {}\n", k.name(), v));
+        } else {
+            out.push_str(&format!("{}{{{}}} {}\n", k.name(), &format_labels(labels), v));
+        }
+    }
+
+    for (k, h) in &report.stats {
+        let name = k.name();
+        let labels = &{
+            let labels = k.labels();
+            if labels.is_empty() {
+                "".to_string()
+            } else {
+                format!(", {}", format_labels(labels))
+            }
+        };
+        out.push_str(&format_stat("count", name, labels, h.count()));
+        out.push_str(&format_stat("mean", name, labels, h.mean()));
+        out.push_str(&format_stat("min", name, labels, h.min()));
+        out.push_str(&format_stat("max", name, labels, h.max()));
+        out.push_str(&format_stat("stddev", name, labels, h.stdev()));
+        out.push_str(&format_stat("p50", name, labels, h.value_at_percentile(0.5)));
+        out.push_str(&format_stat("p90", name, labels, h.value_at_percentile(0.9)));
+        out.push_str(&format_stat("p95", name, labels, h.value_at_percentile(0.95)));
+        out.push_str(&format_stat("p99", name, labels, h.value_at_percentile(0.99)));
+        out.push_str(&format_stat("p999", name, labels, h.value_at_percentile(0.999)));
+        out.push_str(&format_stat("p9999", name, labels, h.value_at_percentile(0.9999)));
+    }
+
+    out
+}
+
+fn format_stat<V: Display>(stat: &str, name: &str, labels: &str, v: V) -> String {
+    let out = format!("{}{{stat=\"{}\"{}}} {}\n", name, stat, labels, v);
+    drop(v); // this is really just to appease clippy.
+    out
+}
+
+fn format_labels(labels: &BTreeMap<String, String>) -> String {
+    let mut out = String::with_capacity(16 * 1024);
+    let sz = labels.len();
+    for (i, (k, v)) in labels.iter().enumerate() {
+        out.push_str(&format!("{}=\"{}\"", k, v));
+        if i < sz - 1 {
+            out.push_str(", ");
+        }
+    }
+    out
+}
