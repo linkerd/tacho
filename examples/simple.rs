4fa6c52cbd2bb79abc4b8d852d92a01e9515e750
Oliver Gould <ver@buoyant.io>
2017-04-20 14:41:11 -0700

    Appease rustfmt (#11)

--- examples/simple.rs
+++ examples/simple.rs
@@ -19,11 +19,11 @@ fn main() {
             .sleep(Duration::from_millis(1000))
             .map_err(|_| {})
             .map(move |_| {
-                let r = reporter.peek();
-                println!("# metrics:");
-                println!("");
-                println!("{}", tacho::prometheus::format(&r));
-            })
+                     let r = reporter.peek();
+                     println!("# metrics:");
+                     println!("");
+                     println!("{}", tacho::prometheus::format(&r));
+                 })
     });
 
     let mut core = Core::new().expect("could not create core");
@@ -35,19 +35,20 @@ fn do_work(metrics: tacho::Scope) -> future::BoxFuture<(), ()> {
     let iter_time_us = metrics.stat("iter_time_us".into());
     let timer = Timer::default();
     future::loop_fn(100, move |n| {
-            // Clones are shallow, minimizing allocation.
-            let mut iter_time_us = iter_time_us.clone();
+        // Clones are shallow, minimizing allocation.
+        let mut iter_time_us = iter_time_us.clone();
 
-            let start = Timing::start();
-            timer.sleep(Duration::from_millis(20 * (n % 5)))
-                .map_err(|_| {})
-                .map(move |_| if n == 0 {
-                    future::Loop::Break(n)
-                } else {
-                    iter_time_us.add(start.elapsed_us());
-                    future::Loop::Continue(n - 1)
-                })
-        })
-        .map(|_| {})
-        .boxed()
+        let start = Timing::start();
+        timer
+            .sleep(Duration::from_millis(20 * (n % 5)))
+            .map_err(|_| {})
+            .map(move |_| if n == 0 {
+                     future::Loop::Break(n)
+                 } else {
+                     iter_time_us.add(start.elapsed_us());
+                     future::Loop::Continue(n - 1)
+                 })
+    })
+            .map(|_| {})
+            .boxed()
 }
